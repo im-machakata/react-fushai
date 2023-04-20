e1aacce560294cb2816e9f524e95efa476e0dba0
Machakata Isaac <isaac@caasi.co.zw>
2023-04-20 03:20:46 +0000

    refactor: input component

---
+++ src/components/Input.tsx
@@ -0,0 +1,19 @@
+import { Component } from "react";
+
+export interface IProps {
+    type: String,
+    placeholder: String,
+}
+
+class Input extends Component<IProps> {
+
+    render() {
+        return (
+           <div>
+               
+           </div>
+        );
+    }
+}
+
+export default Input;
